f69e175bc6e73c8c254b567edd12d2519d83362f
Kaylyn DiCiaccio <kaylyndiciaccio@kaylyns-air.lan>
2021-04-07 17:21:47 -0400

    finished code

---
+++ app/models/crew_member.rb
@@ -0,0 +1,7 @@
+class CrewMember < ActiveRecord::Base
+  belongs_to :starship
+
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+  validates :specialty_division, presence: true
+end
--- server.rb
+++ server.rb
@@ -12,12 +12,12 @@ get '/' do
 end
 
 get '/starships' do
-  
+  @starships = Starship.all
   erb :'starships/index'
 end
 
 get '/crew-members' do
-
+  @crew_members = CrewMember.all
   erb :'crew_members/index'
 end
 
@@ -27,14 +27,45 @@ end
 
 get '/starships/:id' do
 
+
+
+@starship = Starship.find(params["id"])
   erb :'starships/show'
 end
 
 post '/starships' do
+  # binding.pry
+  @name = params["name"]
+  @ship_class = params["ship_class"]
+  @location = params["location"]
+
+  new_starship = Starship.new(name: @name, ship_class: @ship_class, location: @location)
+
+  # new_starship2 = Starship.create(name: @name, ship_class: @ship_class, location: @location)
+# binding.pry
+  if new_starship.save
+    flash[:notice] = "we made a ship, yay!"
+    redirect "/starships/#{new_starship.id}"
+  else
+    flash.now[:notice] = new_starship.errors.full_messages.to_sentence
+    erb :'starships/new'
+  end
 
 
 end
 
 post '/starships/:starship_id/crew-members' do
+  # binding.pry
+
+  new_member = CrewMember.new(params)
+  @starship = Starship.find(params["starship_id"])
+
+  if new_member.save
+    flash[:notice] = "crew member added"
+    redirect "/crew-members"
+  else
+    flash.now[:notice] = new_member.errors.full_messages.to_sentence
+    erb :'starships/show'
+  end
 
 end
