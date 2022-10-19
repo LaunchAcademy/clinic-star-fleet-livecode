99274987848d00525e1722fb27b82472209a1e8e
Nick Alberts <nwalberts5@gmail.com>
2022-10-19 16:02:17 -0400

    full livecode base

--- app/models/crew_member.rb
+++ app/models/crew_member.rb
@@ -1,9 +1,3 @@
 class CrewMember < ActiveRecord::Base 
 
-    validates :first_name, presence: true
-    validates :last_name, presence: true
-    validates :star_ship_id, presence: true
-
-    belongs_to :star_ship
-
 end
--- app/models/star_ship.rb
+++ app/models/star_ship.rb
@@ -1,9 +1,3 @@
 class StarShip < ActiveRecord::Base 
-    validates :name, presence: true
-    validates :location, presence: true
-    validates :ship_class, presence: true
-
-    has_many :crew_members
 
 end
-#  name, ship class, and location of the starship.
